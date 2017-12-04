0be8986c0facdeb20b7e4644ae49d8e1e5becc53
Christophe de Batz <christophe.db@gmail.com>
2017-12-04 01:28:25 +0100

    feat(cigarette): add date filter to cigarettes fetching endpoint

--- src/model/dao/CigaretteDao.ts
+++ src/model/dao/CigaretteDao.ts
@@ -8,23 +8,20 @@ export const CigaretteDao = {
   getUserCigarettes(userFbId:string, from:Date|undefined, to:Date|undefined):Promise<Cigarette[]> {
     return Database.getInstance()
       .then(async connection => {
-        const l = await connection.getRepository(Cigarette)
+        const qb = await connection.getRepository(Cigarette)
           .createQueryBuilder('c')
           .innerJoin('c.user', 'user')
           .where('user.fbId = :userFbId', { userFbId });
 
-        return l.getMany();
+        if (from) {
+          qb.andWhere('creation_date > :from', { from });
+        }
+        
+        if (to) {
+          qb.andWhere('creation_date < :to', { to });
+        }
 
-        // if (from) {
-        //   qb.andWhere('creation_date > :from', { from });
-        // }
-        // if (to) {
-        //   qb.andWhere('creation_date < :to', { to });
-        // }
-        // console.log('sql=', qb.getSql());
-        // const l = qb.getMany();
-        // console.log('l=', l);
-        // return l;
+        return qb.getMany();
       });
   }
 }
