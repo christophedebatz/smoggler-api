0a8dfd33dc18d68a924868d30b78a889c48090fe
Christophe de Batz <christophe.db@gmail.com>
2018-01-02 11:54:11 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,3 +12,24 @@ export let settings: Config = {
 if (env === 'production') {
   settings.env = 'prod';
 }
+
+let databaseSettings = {
+  dev: {
+    host: 'localhost',
+    port: 3307,
+    username: 'root',
+    password: '',
+    database: 'smoggler'
+  },
+  'prod': {
+    host: '172.18.0.2',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'smoggler'
+  }
+};
+
+if (databaseSettings[settings.env]) {
+  settings.database = databaseSettings[settings.env];
+}
